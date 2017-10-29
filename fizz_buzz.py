6ad40ffb588fe0840f406189c95e1f221797ff61
niraj36 <niraj36@dmazurevm1.(none)>
2017-10-29 22:37:08 +0000

    Tinkering with formatting.

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,4 +1,4 @@
-def fizz_buzz (start_value, end_value, fizz_value, buzz_value):
+def fizz_buzz(start_value, end_value, fizz_value, buzz_value):
     count = start_value
     message = count
     while count <= end_value:
@@ -13,4 +13,4 @@ def fizz_buzz (start_value, end_value, fizz_value, buzz_value):
         message = count
 
 
-fizz_buzz(1,100,3,5)
+fizz_buzz(1, 100, 3, 5)
