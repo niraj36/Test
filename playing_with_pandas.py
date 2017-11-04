59bcae51af68cf34807ff53b987dd2ff45d1ae62
niraj36 <niraj36@dmazurevm1.(none)>
2017-11-04 18:46:14 +0000

    More tinkering.

---
+++ playing_with_pandas.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import fizz_buzz as fb
+
+data = pd.read_csv('C:/_data/Portfolios.csv')
+print(data)
+
+fizzy = fb.fizz_buzz(1, 10000, 3, 5)
+print(fizzy)
+
+return_fields = ['Symbol','Last']
+prices = pd.read_csv('C:/_data/Portfolios.csv',usecols=return_fields).drop_duplicates(subset=['Symbol', 'Last'], keep='first')
+print(prices)
