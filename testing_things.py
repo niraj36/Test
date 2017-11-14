fd4e573b51a57329c4d5c45bf0ea476287bf5a06
niraj36 <niraj36@dmazurevm1.(none)>
2017-11-14 12:43:03 +0000

    tweak

--- testing_things.py
+++ testing_things.py
@@ -11,11 +11,3 @@ print(fizzy)
 return_fields = ['Symbol','Last']
 prices = pd.read_csv('C:/_data/Portfolios.csv',usecols=return_fields).drop_duplicates(subset=['Symbol', 'Last'], keep='first')
 print(prices)
-
-
-uri = open('C:/_data/DatabaseConnectionSting_test.txt', 'r').read()
-print(uri)
-
-import os, sys
-qq = sys.path.append(os.getcwd())
-print(qq)
