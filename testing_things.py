d7777802d83b7d3b0c43339d5b1d5a1b4bbe91a5
niraj36 <niraj36@dmazurevm1.(none)>
2017-11-19 22:34:21 +0000

    rename project test

--- testing_things.py
+++ testing_things.py
@@ -9,5 +9,8 @@ return_fields = ['Symbol','Last']
 prices = pd.read_csv('C:/_data/Portfolios.csv',usecols=return_fields).drop_duplicates(subset=['Symbol', 'Last'], keep='first')
 
 print(prices)
+print(prices)
 print(fizzy)
+print(fizzy)
+print(data)
 print(data)
