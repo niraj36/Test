e5c40252d9d67a33021df36ba0b94a8d33c2aa85
niraj36 <niraj36@dmazurevm1.(none)>
2017-11-19 22:33:05 +0000

    random update

--- testing_things.py
+++ testing_things.py
@@ -3,11 +3,11 @@ import fizz_buzz as fb
 
 
 data = pd.read_csv('C:/_data/Portfolios.csv')
-print(data)
-
 fizzy = fb.fizz_buzz(1, 10000, 3, 5)
-print(fizzy)
 
 return_fields = ['Symbol','Last']
 prices = pd.read_csv('C:/_data/Portfolios.csv',usecols=return_fields).drop_duplicates(subset=['Symbol', 'Last'], keep='first')
+
 print(prices)
+print(fizzy)
+print(data)
